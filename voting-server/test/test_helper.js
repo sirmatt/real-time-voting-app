4a82e5b39e93d215a8adf5384313313c17ec28e0
Matt Batista <mattbgroovy@gmail.com>
2016-08-02 15:13:17 -0700

    Setup mocha framework with chai extension

---
+++ voting-server/test/test_helper.js
@@ -0,0 +1,10 @@
+/* jshint esversion: 6 */
+
+/*
+'chai-immutable' extends Chai to add support for comparing FB's Immutable data structures
+*/
+
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
+
+chai.use(chaiImmutable);
