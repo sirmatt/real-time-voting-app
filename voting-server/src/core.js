97cb15cf467c0c5791b95935de89c2d34a325314
Matt Batista <mattbgroovy@gmail.com>
2016-08-03 14:13:54 -0700

    create test for next function and make pass

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -1,5 +1,17 @@
 /* jshint esversion: 6 */
+import {List, Map} from 'immutable';
 
 export function setEntries(state, entries) {
-  return state.set('entries', entries);
+  return state.set('entries', List(entries));
+}
+
+export function next(state) {
+  // Grabs the Immutable List of movies in the state
+  const entries = state.get('entries');
+  return state.merge({
+    vote: Map({
+      pair: entries.take(2)
+    }),
+    entries: entries.skip(2)
+  });
 }
--- voting-server/test/core_logic_spec.js
+++ voting-server/test/core_logic_spec.js
@@ -4,11 +4,11 @@ import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
 // imports function from file path specified
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () => {
 
-  describe('setEntries', () => {
+  describe('setEntries function', () => {
 
     it('adds the entries to the state', () => {
       const state = Map();
@@ -20,6 +20,34 @@ describe('application logic', () => {
       }));
     });
 
+    it('converts JS array to immutable List', () => {
+      const state = Map();
+      const entries = ['Kill Bill', 'Pulp Fiction'];
+      const nextState = setEntries(state, entries);
+
+      expect(nextState).to.equal(Map({
+        entries: List.of('Kill Bill', 'Pulp Fiction')
+      }));
+    });
   });
+
+  describe('next function', () => {
+
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Kill Bill', 'Pulp Fiction', 'Jackie Brown')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Kill Bill', 'Pulp Fiction')
+        }),
+        entries: List.of('Jackie Brown')
+      }));
+    });
+
+  });
+
+
 
 });
