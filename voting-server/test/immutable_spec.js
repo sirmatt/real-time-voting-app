7d54f1d23fce9d1a1bc8ca095cbeee094cdf6087
Matt Batista <mattbgroovy@gmail.com>
2016-08-03 13:01:30 -0700

    Write tests to get familiar with FB's Immutable data structure

---
+++ voting-server/test/immutable_spec.js
@@ -0,0 +1,74 @@
+/* jshint esversion: 6 */
+import {expect} from 'chai';
+import {List, Map} from 'immutable';
+
+describe('Immutability', () => {
+
+  describe('an immutable number', () => {
+
+    function increment(currentState) {
+      return currentState + 1;
+    }
+
+    it('is immutable', () => {
+      let state = 42;
+      let nextState = increment(state);
+
+      expect(nextState).to.equal(43);
+      expect(state).to.equal(42);
+    });
+
+  });
+
+  describe('an immutable list(array)', () => {
+    function addMovie(currentState, newMovie) {
+      return currentState.push(newMovie);
+    }
+
+    it('is immutable fersure', () => {
+      let state = List.of('Reservoir Dogs', 'Pulp Fiction');
+      let nextState = addMovie(state, 'Kill Bill');
+
+      expect(nextState).to.equal(List.of(
+        'Reservoir Dogs',
+        'Pulp Fiction',
+        'Kill Bill'
+      ));
+
+      expect(state).to.equal(List.of(
+        'Reservoir Dogs',
+        'Pulp Fiction'
+      ));
+
+    });
+  });
+
+  describe('an immutable tree(map)', () => {
+
+    function addMovie(currentState, newMovie) {
+      return currentState.set(
+        'movies',
+        currentState.get('movies').push(newMovie));
+    }
+
+    it('is immutable fersure', () => {
+      let state = Map({
+        movies: List.of('Kill Bill', 'Pulp Fiction')
+      });
+      let nextState = addMovie(state, '300');
+
+      expect(nextState).to.equal(Map({
+        movies: List.of(
+          'Kill Bill',
+          'Pulp Fiction',
+          '300'
+        )
+      }));
+      expect(state).to.equal(Map({
+        movies: List.of('Kill Bill', 'Pulp Fiction')
+      }));
+    });
+    
+  });
+
+});
